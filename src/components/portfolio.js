3df9434e9b5e0cfafb88e33db10d53be61a364c4
unknown <kcavner@hotmail.com>
2023-05-25 21:49:37 -0500

    fixed portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,57 +1,59 @@
 import React from 'react';
 import PortfolioCard from './portfolioCard';
-
+import forumImage from '../assets/forum.png';
+import weatherImage from '../assets/weather.JPG';
+import movieImage from '../assets/movie.jpg';
+import raycastImage from '../assets/raycast.JPG'; // Assuming the file extension is '.jpg'
+import abcDojoImage from '../assets/abcdojo.JPG';
+import ezlImage from '../assets/ezl.jpg';
 
 const portfolioItems = [
-    {
-      id: 1,
-      imageSrc:'../assets/forum.png',
-      github: 'https://github.com/amsudekum/Forum-Project',
-    },
-    {
-      id: 2,
-      imageSrc: '../assets/weather.JPG',
-      github: 'https://github.com/kcavner/weather-dashboard',
-    },
-    {
-      id: 3,
-      imageSrc: '../assets/movie.JPG',
-      github: 'https://github.com/jboyce313/movie-game',
-    },
-    {
-      id: 4,
-      imageSrc: '../assets/raycast.JPG',
-      github: 'https://github.com/kcavner/raycast-visualizer',
-    },
-    {
-      id: 5,
-      imageSrc: '../assets/abcdojo.JPG',
-      github: 'https://kcavner.github.io/abc-dojo/',
-    },
-    {
-      id: 6,
-      imageSrc: '../assets/ezl.jpg',
-      github: 'https://github.com/kcavner/Ezleagues',
-    },
-  ];
+  {
+    id: 1,
+    imageSrc: forumImage,
+    github: 'https://github.com/amsudekum/Forum-Project',
+  },
+  {
+    id: 2,
+    imageSrc: weatherImage,
+    github: 'https://github.com/kcavner/weather-dashboard',
+  },
+  {
+    id: 3,
+    imageSrc: movieImage,
+    github: 'https://github.com/jboyce313/movie-game',
+  },
+  {
+    id: 4,
+    imageSrc: raycastImage,
+    github: 'https://github.com/kcavner/raycast-visualizer',
+  },
+  {
+    id: 5,
+    imageSrc: abcDojoImage,
+    github: 'https://kcavner.github.io/abc-dojo/',
+  },
+  {
+    id: 6,
+    imageSrc: ezlImage,
+    github: 'https://github.com/kcavner/Ezleagues',
+  },
+];
 
-function Portfolio(){
-    return (
-        <div className='portfolioName'>My Projects
-        <div className='portfolioPage'>
-             {portfolioItems.map((item) => (
-        <PortfolioCard
-          key={item.id}
-          imageSrc={item.imageSrc}
-          github={item.github}
-          
-        />
-      ))}
-           
-        </div>
-        </div>
-    )
+function Portfolio() {
+  return (
+    <div className='portfolioName'>My Projects
+      <div className='portfolioPage'>
+        {portfolioItems.map((item) => (
+          <PortfolioCard
+            key={item.id}
+            imageSrc={item.imageSrc}
+            github={item.github}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
-
--- src/components/portfolioCard.js
+++ src/components/portfolioCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import cat from '../assets/github.png'
 const PortfolioCard = ({ imageSrc, github }) => {
   const openGithub = () => {
     window.open(github, '_blank');
@@ -10,7 +10,7 @@ const PortfolioCard = ({ imageSrc, github }) => {
       <img src={imageSrc} alt="portfolio item" onClick={openGithub} style={{position:"absolute",width: "20vw", height: "15vw" }} onMouseOver={(e) =>{e.currentTarget.style.opacity = .4;}} onMouseOut={(e) => {
       e.currentTarget.style.opacity = 1;
     }} />
-      <img src="../assets/github.png"alt="portfolio item" style={{width: "20vw", height: "15vw" }}/>
+      <img src={cat}alt="portfolio item" style={{width: "20vw", height: "15vw" }}/>
     </div>
   );
 };
